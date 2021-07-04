f0be97d1a306c1853cab7c25dfee2630b543a510
ooookai <ooookai@gmail.com>
2021-07-05 00:06:56 +0800

    init: FaIcon

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/css/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// components
+import FaIcon from '@/components/FaIcon'
+app.component('FaIcon', FaIcon)
+
+app.mount('#app')
